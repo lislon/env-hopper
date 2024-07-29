9f14b29d5e4c574e296a69666fc492c2064d26ed
Igor Golovin <lislon@users.noreply.github.com>
2024-07-28 20:12:48 -0500

    Pass non-empty version in UI (#3)
    * fix: display correct version
    * fix: Better release messages

--- .releaserc.mjs
+++ .releaserc.mjs
@@ -1,8 +1,11 @@
 /**
  * @type {import('semantic-release').GlobalConfig}
+ * @link https://semantic-release.gitbook.io/semantic-release/usage/configuration
  */
 export default {
   plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
     // to get docker container export image
     // https://github.com/felipecrs/semantic-release-export-data#github-actions-example
     'semantic-release-export-data',

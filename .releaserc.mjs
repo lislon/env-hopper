3358764d7d46b57c50380625e78157436a65b8e1
Igor Golovin <lislon@mail.ru>
2024-07-28 20:16:26 -0500

    chore: Trigger new version

--- .releaserc.mjs
+++ .releaserc.mjs
@@ -6,7 +6,6 @@ export default {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    // to get docker container export image
     // https://github.com/felipecrs/semantic-release-export-data#github-actions-example
     'semantic-release-export-data',
     '@semantic-release/github'

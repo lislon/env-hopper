43d7d55876aa44c78751ec74d31a4b3bbdf3c95d
Igor Golovin <lislon@mail.ru>
2024-10-14 22:03:35 -0500

    fix: Fix DB migration

--- apps/backend/prisma/migrations/20241013043213_custom_meta/migration.sql
+++ apps/backend/prisma/migrations/20241013043213_custom_meta/migration.sql
@@ -1,9 +1,3 @@
-/*
-  Warnings:
-
-  - Added the required column `unstable__custom` to the `UnstableCustomization` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- RedefineTables
 PRAGMA defer_foreign_keys=ON;
 PRAGMA foreign_keys=OFF;
@@ -11,7 +5,7 @@ CREATE TABLE "new_UnstableCustomization" (
     "syntheticId" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "unstable__analytics_script" TEXT NOT NULL,
     "unstable__footer_html" TEXT NOT NULL,
-    "unstable__custom" TEXT NOT NULL
+    "unstable__custom" TEXT NOT NULL DEFAULT '{}'
 );
 INSERT INTO "new_UnstableCustomization" ("syntheticId", "unstable__analytics_script", "unstable__footer_html") SELECT "syntheticId", "unstable__analytics_script", "unstable__footer_html" FROM "UnstableCustomization";
 DROP TABLE "UnstableCustomization";

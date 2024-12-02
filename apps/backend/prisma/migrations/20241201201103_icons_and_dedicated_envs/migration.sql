aec51835e35598dc562684e523fd08c510f9a4a6
Igor Golovin <lislon@users.noreply.github.com>
2024-12-02 14:45:36 -0600

    fix: Fix migration script (#257)

--- apps/backend/prisma/migrations/20241201201103_icons_and_dedicated_envs/migration.sql
+++ apps/backend/prisma/migrations/20241201201103_icons_and_dedicated_envs/migration.sql
@@ -5,10 +5,6 @@
   - Added the required column `unstable__icons` to the `UnstableCustomization` table without a default value. This is not possible if the table is not empty.
 
 */
--- AlterTable
-ALTER TABLE "Environment" ADD COLUMN "appOverride" TEXT;
-ALTER TABLE "Environment" ADD COLUMN "envType" TEXT;
-ALTER TABLE "Application" ADD COLUMN "widgets" TEXT;
 
 -- RedefineTables
 PRAGMA defer_foreign_keys=ON;
@@ -21,8 +17,14 @@ CREATE TABLE "new_UnstableCustomization" (
     "unstable__icons" TEXT NOT NULL,
     "unstable__appLinkTypes" TEXT NOT NULL
 );
-INSERT INTO "new_UnstableCustomization" ("syntheticId", "unstable__analytics_script", "unstable__custom", "unstable__footer_html") SELECT "syntheticId", "unstable__analytics_script", "unstable__custom", "unstable__footer_html" FROM "UnstableCustomization";
+INSERT INTO "new_UnstableCustomization" ("syntheticId", "unstable__analytics_script", "unstable__custom", "unstable__footer_html", "unstable__appLinkTypes", "unstable__icons")
+SELECT "syntheticId", "unstable__analytics_script", "unstable__custom", "unstable__footer_html", '[]' AS unstable__appLinkTypes, '{}' AS unstable__icons FROM "UnstableCustomization";
 DROP TABLE "UnstableCustomization";
 ALTER TABLE "new_UnstableCustomization" RENAME TO "UnstableCustomization";
 PRAGMA foreign_keys=ON;
 PRAGMA defer_foreign_keys=OFF;
+
+-- AlterTable
+ALTER TABLE "Environment" ADD COLUMN "appOverride" TEXT;
+ALTER TABLE "Environment" ADD COLUMN "envType" TEXT;
+ALTER TABLE "Application" ADD COLUMN "widgets" TEXT;

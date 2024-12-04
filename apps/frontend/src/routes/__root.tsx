cb96298d8b889b14add1e022cc0ec193a228dc76
Igor Golovin <lislon@mail.ru>
2024-12-04 12:55:33 -0600

    fix: Cache was not used, which made app slow to load

--- apps/frontend/src/routes/__root.tsx
+++ apps/frontend/src/routes/__root.tsx
@@ -5,9 +5,16 @@ import { NotFoundError } from '../app/ui/Error/NotFoundError';
 import { LoadingScreen } from '../app/ui/Layout/LoadingScreen';
 import { DefaultErrorPage } from '../app/ui/Error/DefaultErrorPage';
 import { EhContextProvider } from '../app/context/EhContext';
+import { useQuery } from '@tanstack/react-query';
+import { ApiQueryMagazine } from '../app/api/ApiQueryMagazine';
 
 export const Route = createRootRoute({
   component: () => {
+    if (useQuery(ApiQueryMagazine.getConfig()).isPending) {
+      // workaround https://github.com/TanStack/query/issues/8400
+      return <LoadingScreen />;
+    }
+
     return (
       <EhContextProvider>
         <Outlet />
